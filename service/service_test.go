fa1acb191d3140f98d6fb75f39aabf8aa144bc8d
Ignacio Hagopian <jsign.uy@gmail.com>
2022-04-27 14:30:03 -0300

    remove boost detection helper
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- service/service_test.go
+++ service/service_test.go
@@ -309,7 +309,6 @@ func newLotusClientMock() *lotusclientmocks.LotusClient {
 		mock.Anything,
 		mock.Anything,
 	).Return(nil)
-	lc.On("IsRunningBoost").Return(false, nil)
 	lc.On("Close").Return(nil)
 	return lc
 }
